142816751bcb0b34dbabefac74022d73a7fee208
BuildTools <davidliepic@gmail.com>
2021-02-22 21:46:13 -0500

    update the README.md with link to contributing guide

--- life/__main__.py
+++ life/__main__.py
@@ -1,1 +1,67 @@
-print("hello")
+import pygame
+import sys
+from button import Button
+
+# Screen size and initialization of pygame
+pygame.init()
+width, height = 1280, 720
+size = [width, height]
+screen = pygame.display.set_mode(size)
+
+# Colors
+black = [0, 0, 0]
+red = [255, 0, 0]
+light_red = [200, 10, 10]
+
+play = 0
+
+def print_test():
+    print('test')
+
+game_back_button = Button(
+    red,
+    light_red,
+    15,
+    10,
+    75,
+    35,
+    "Exit",
+    font=30,
+    padding=0,
+    on_click=print_test,
+)
+
+# Game loop
+while True:
+
+    # Menu loop
+    while play == 0:
+
+        # Checking for user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            game_back_button.handle_event(event)
+
+        
+        screen.fill(black)
+
+        game_back_button.draw(screen)
+
+        # Updating the display
+        pygame.display.update()
+
+        # Delay to save system resources
+        pygame.time.wait(5)
+
+    # Transition loop
+    while play == 1:
+
+        screen.fill(black)
+
+        # Updating the display
+        pygame.display.update()
+
+        # Delay to save system resources
+        pygame.time.wait(5)
---
+++ life/button.py
@@ -0,0 +1,98 @@
+import pygame
+
+# Button class for easier generation of buttons
+class Button:
+    def __init__(
+        self,
+        primary,
+        hover_color,
+        x,
+        y,
+        width,
+        height,
+        text,
+        font=18,
+        padding=13,
+        secondary_text="",
+        on_click=None,
+        font_color=[255, 255, 255],
+    ):
+        self.primary = primary
+        self.hover_color = hover_color
+        self.color = self.primary
+        self.text = text
+        self.on_click = on_click
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.secondary_text = secondary_text
+        self.font = font
+        self.font_color = font_color
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.padding = padding
+
+    def draw(self, screen, outline=None):
+        # Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(
+                screen,
+                outline,
+                (self.x - 2, self.y - 2, self.width + 4, self.height + 4),
+                0,
+            )
+
+        # Drawing button rect
+        pygame.draw.rect(screen, self.color, self.rect)
+
+        # Placing primary text on button
+        if self.text != "":
+            font = pygame.font.Font("pixel.ttf", self.font)
+            text = font.render(self.text, 1, self.font_color)
+            screen.blit(
+                text,
+                (
+                    self.x + (self.width / 2 - text.get_width() / 2),
+                    self.y
+                    + (self.height / 2 - text.get_height() / 2)
+                    - self.padding
+                    + 4,
+                ),
+            )
+
+        # Placing secondary text on button with offset
+        if self.secondary_text != "":
+            font = pygame.font.Font("pixel.ttf", self.font)
+            text = font.render(self.secondary_text, 1, self.font_color)
+            screen.blit(
+                text,
+                (
+                    self.x + (self.width / 2 - text.get_width() / 2),
+                    self.y
+                    + (self.height / 2 - text.get_height() / 2)
+                    + self.font
+                    + 5
+                    - self.padding
+                    + 4,
+                ),
+            )
+
+    # Handling button click
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Gets mouse position
+            mouse_pos = event.pos
+
+            # Checks to see if the mouse pressed the button
+            if self.rect.collidepoint(mouse_pos):
+                if self.on_click != None:
+                    self.on_click()
+        elif event.type == pygame.MOUSEMOTION:
+            # Gets mouse position
+            mouse_pos = event.pos
+
+            # Checks to see if the mouse pressed the button
+            if self.rect.collidepoint(mouse_pos):
+                self.color = self.hover_color
+            else:
+                self.color = self.primary
